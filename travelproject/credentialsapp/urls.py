ddb2949e342c547b6d667b35cd6474bc70bfdb5f
nimapython <nimanandan8@gmail.com>
2023-03-12 14:45:25 +0530

    added

---
+++ travelproject/credentialsapp/urls.py
@@ -0,0 +1,15 @@
+from xml.etree.ElementInclude import include
+
+
+
+from.import views
+
+from django.urls import path
+
+urlpatterns = [
+
+    path('register',views.register,name='register'),
+    path('login',views.login,name='login'),
+    path('logout',views.logout,name='logout'),
+]
+
